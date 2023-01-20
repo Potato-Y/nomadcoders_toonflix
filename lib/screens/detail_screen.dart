a687ab41d309a5c571cce5bc2f2ce58b0a13d4e7
Potato-Y <projectpotatoy@gmail.com>
2023-01-20 14:52:42 +0900

    화면 전환 효과 추가

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -33,19 +33,22 @@ class DetailScreen extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 15,
-                        offset: const Offset(10, 10),
-                        color: Colors.black.withOpacity(0.3),
-                      ),
-                    ]),
-                width: 250,
-                child: Image.network(thumb),
+              Hero(
+                tag: id,
+                child: Container(
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 15,
+                          offset: const Offset(10, 10),
+                          color: Colors.black.withOpacity(0.3),
+                        ),
+                      ]),
+                  width: 250,
+                  child: Image.network(thumb),
+                ),
               ),
             ],
           ),
